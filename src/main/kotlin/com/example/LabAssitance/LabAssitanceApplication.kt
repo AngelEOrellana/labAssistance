9e701dbae5d840848c13e770578aa5f8df011085
AngelEOrellana <71674390+AngelEOrellana@users.noreply.github.com>
2023-11-17 20:38:31 -0500

    creacion proyecto

---
+++ src/main/kotlin/com/example/LabAssitance/LabAssitanceApplication.kt
@@ -0,0 +1,11 @@
+package com.example.LabAssitance
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class LabAssitanceApplication
+
+fun main(args: Array<String>) {
+	runApplication<LabAssitanceApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/LabAssitance/LabAssitanceApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.LabAssitance
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class LabAssitanceApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
